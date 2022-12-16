322f98682fa92f7ae0dc2b178828ce1bfc7a8356
catherine <h1codingqueenh1@gmail.com>
2022-12-15 20:27:32 -0500

    Initiated Project

---
+++ Memorise/MemoriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriseApp.swift
+//  Memorise
+//
+//  Created by adobada on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct MemoriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
