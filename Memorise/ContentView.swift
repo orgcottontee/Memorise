4bf05a588eb8c370912f6cb61d8cada97dda948b
catherine <h1codingqueenh1@gmail.com>
2022-12-15 20:59:53 -0500

    Created UI

--- Memorise/ContentView.swift
+++ Memorise/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    var emojis = ["🚝", "🚖", "🚡", "🚔", "🚃", "🚈", "🚂", "🚆", "✈️", "⛵️", "🚀", "🛰️", "🛸", "🚁", "🛳️", "🛥️", "🛩️", "🚅", "🚍", "🛵", "🚑", "🚒", "🚜", "🏎️"]
+    @State var emojiCount = 20
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]) {
+                    ForEach(emojis[0..<emojiCount], id: \.self) { emoji in
+                        CardView(content:emoji)
+                            .aspectRatio(2/3, contentMode: .fit)
+                    }
+                }
+            }
+            .foregroundColor(.red)
         }
-        .padding()
+        .padding(.horizontal)
+    }
+}
+
+struct CardView: View {
+    var content: String
+    @State var isFaceUp: Bool = true
+    
+    var body: some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: 20)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: 3)
+                Text(content).font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp = !isFaceUp
+        }
     }
 }
 
