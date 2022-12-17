2114e5fac6d529350eb37ef9a4373d62bd5c04a4
catherine <h1codingqueenh1@gmail.com>
2022-12-16 19:17:04 -0500

    Completed infrastructure of MemoryGame and EmojiMemoryGame

--- Memorise/EmojiMemoryGame.swift
+++ Memorise/EmojiMemoryGame.swift
@@ -4,6 +4,29 @@
 //
 //  Created by adobada on 12/16/22.
 //
+// This is the ViewModel, and the job is to be the intermediary between the Model and the View
+// As a result, the ViewModel is part of the UI, so we can import SwiftUI
+// ViewModel's role as gatekeep to the Model - it's the ViewModel's job to protect the model against ill-behaving Views/anyone who has access to the ViewModel
+// instance member means any function or variable that is defined in your emojiMemoryGame
+// property refers to vars or lets inside a Struct or Class
+// initilizer is when you assign a value to a property using an equal sign
+// type function or type property is when you use static before a property or function. Using static makes the function or property global, but keeps them namespaced inside our class. Result is that the type function and property are attached to the type and created one(EmojiMemoryGame in this case, which is why it is called a type function or property) whereas a regular property or function is on every instance -
 
 import SwiftUI
-// 
+
+class EmojiMemoryGame {
+    static let emojis = ["🚝", "🚖", "🚡", "🚔", "🚃", "🚈", "🚂", "🚆", "✈️", "⛵️", "🚀", "🛰️", "🛸", "🚁", "🛳️", "🛥️", "🛩️", "🚅", "🚍", "🛵", "🚑", "🚒", "🚜", "🏎️"]
+    
+    static func createMemoryGame() -> MemoryGame<String> {
+        MemoryGame<String>(numberOfPairsOfCards: 4) { pairIndex in
+            emojis[pairIndex]
+        }
+    }
+    private var model: MemoryGame<String> = createMemoryGame()
+    // This is where we explicity delcare the type of the "don't care/generic" (<CardContent>)of MemoryGame
+    // 'private' means that only the ViewModel's code itself can see Model. Doing so protects the model against any View reaching in and tyrihng to change things -> part of something called access control. Make it private unless we're SURE we want other Structs and Classes to access them.
+    // 'private(set)' tells Swift that other Classes and Structs can look at the Model, but can't change it
+    var cards: Array<MemoryGame<String>.Card> {
+        return model.cards
+    }
+}
--- Memorise/MemoryGame.swift
+++ Memorise/MemoryGame.swift
@@ -4,6 +4,7 @@
 //
 //  Created by adobada on 12/16/22.
 //
+//
 
 import Foundation
 
@@ -13,10 +14,24 @@ struct MemoryGame<CardContent> {
     func choose(_ card: Card) {
         
     }
+    // By nesting this Struct, we make it clear that this is a Card that belongs to a MemoryGame
+    
+    //Using generics and functions as arguments 
+    init(numberOfPairsOfCards: Int, createCardContent: (Int) -> CardContent) {
+        cards = Array<Card>()
+        // add numberOfPairsOfCards x 2 card to cards array
+        // We lose the free init when we declare one here
+        for pairIndex in 0..<numberOfPairsOfCards {
+            let content = createCardContent(pairIndex)
+            cards.append(Card(content: <#T##CardContent#>))
+            cards.append(Card(content: <#T##CardContent#>))
+        }
+    }
     
     struct Card {
-        var isFaceUp: Bool
-        var isMatched: Bool
+        var isFaceUp: Bool = false
+        var isMatched: Bool = false
         var content: CardContent
+        // We made the content generic (don't care) beacuse we don't want to limit the content to only have String types (our app uses emojis). Since we're using a "don't care," we need to announce it to the world by including it within angle brackets in the main Struct because when someone uses the main Struct, they're gonna have to explicit tell us the type for the "don't care."
     }
 }
